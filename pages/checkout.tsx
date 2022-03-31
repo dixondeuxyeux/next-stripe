9d424a7aed78e5915844f8b35cf3ee9aede23030
dixondeuxyeux <dixondeuxyeux@gmail.com>
2022-03-31 12:45:33 +0000

    changes to typescript and add checkout

---
+++ pages/checkout.tsx
@@ -0,0 +1,29 @@
+import {loadStripe} from '@stripe/stripe-js'
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
+
+export default function Checkout() {
+   const handleClick = async (event) => {
+      // call backend to create the checkout session
+      const {sessionId} = await fetch('/api/checkout/session', {
+         method: 'POST',
+         headers: {
+            "content-type": "application/json"
+         },
+         body: JSON.stringify({quantity: 1})
+      }).then(res => res.json())
+      // redirect to checkout
+      const stripe = await stripePromise;
+      const {error} = await stripe.redirectToCheckout({
+         sessionId,
+      })
+   }
+
+   return (
+      <div>
+   <h1>Checkout</h1>
+   <button role='link' onClick={handleClick}>
+      checkout
+   </button>
+   </div>)
+}
